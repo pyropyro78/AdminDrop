f91bf5ecec3033b9fd043c7473c81c1b9cc06953
pyropyro78 <pyropyro78@gmail.com>
2014-01-31 10:54:48 -0800

    Added Metrics Support

--- src/com/pyromanticgaming/admindrop/AdminDrop.java
+++ src/com/pyromanticgaming/admindrop/AdminDrop.java
@@ -12,6 +12,8 @@ package com.pyromanticgaming.admindrop;
 *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+import java.io.IOException;
+
 import org.bukkit.ChatColor;
 import org.bukkit.block.Chest;
 import org.bukkit.block.DoubleChest;
@@ -35,6 +37,14 @@ public final class AdminDrop extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
+		
+		try {
+		    MetricsLite metrics = new MetricsLite(this);
+		    metrics.start();
+		} catch (IOException e) {
+		    // Failed to submit the stats :-(
+		}
+		
 		getLogger().info("AdminDrop has been enabled.");
 
 		getServer().getPluginManager().registerEvents(this, this);
