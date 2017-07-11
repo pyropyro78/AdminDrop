b8877f133ef93ce1544a76d82f0f498b2b2ada66
Bradley Van Dyne <pyropyro78@gmail.com>
2017-07-10 21:41:36 -0700

    Started moving things from main file - Edits from phone

--- src/com/pyromanticgaming/admindrop/AdminDrop.java
+++ src/com/pyromanticgaming/admindrop/AdminDrop.java
@@ -71,85 +71,9 @@ public final class AdminDrop extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		String p = e.getPlayer().getName();
+JoinToggles.main(p);
 
-
-		if (!PlayerToggles.listedDropAccess(p)) {
-
-			if (e.getPlayer().hasPermission("AdminDrop.alwayson.deathdrop") && !e.getPlayer().hasPermission("AdminDrop.ignore.star.deathdrop")) {
-
-				PlayerToggles.dropaccess.put(p, true);
-
-			} else {
-
-				PlayerToggles.dropaccess.put(p, false);
-
-			}
-		}
-		if (!PlayerToggles.listedThrowAccess(p)) {
-
-			if (e.getPlayer().hasPermission("AdminDrop.alwayson.throwaway") && !e.getPlayer().hasPermission("AdminDrop.ignore.star.throwaway")) {
-
-				PlayerToggles.throwaccess.put(p, true);
-
-			} else {
-
-				PlayerToggles.throwaccess.put(p, false);
-
-			}
-
-		}
-		if (!PlayerToggles.listedPickUpAccess(p)) {
-
-			if (e.getPlayer().hasPermission("AdminDrop.alwayson.pickup") && !e.getPlayer().hasPermission("AdminDrop.ignore.star.pickup")) {
-
-				PlayerToggles.pickupaccess.put(p, true);
-
-			} else {
-
-				PlayerToggles.pickupaccess.put(p, false);
-
-			}
-
-		}
-		if (!PlayerToggles.listedChestAccess(p)) {
-
-			if (e.getPlayer().hasPermission("AdminDrop.alwayson.chestaccess") && !e.getPlayer().hasPermission("AdminDrop.ignore.star.chestaccess")) {
-
-				PlayerToggles.chestaccess.put(p, true);
-
-			} else {
-
-				PlayerToggles.chestaccess.put(p, false);
-
-			}
-
-		}
-		if (!PlayerToggles.listedBlockBreak(p)) {
-
-			if (e.getPlayer().hasPermission("AdminDrop.alwayson.block.break") && !e.getPlayer().hasPermission("AdminDrop.ignore.star.block.break")) {
-
-				PlayerToggles.blockbreak.put(p, true);
-
-			} else {
-
-				PlayerToggles.blockbreak.put(p, false);
-
-			}
-
-		}
-		if (!PlayerToggles.listedBlockPlace(p)) {
-
-			if (e.getPlayer().hasPermission("AdminDrop.alwayson.block.place") && !e.getPlayer().hasPermission("AdminDrop.ignore.star.block.place")) {
-
-				PlayerToggles.blockplace.put(p, true);
-
-			} else {
-
-				PlayerToggles.blockplace.put(p, false);
-
-			}
-
-		}
+		
 	}
 
 	
